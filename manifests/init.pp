b03b16896689e5907bff8215828298026adb84c5
David <davidnewton82@gmail.com>
2015-11-30 17:25:50 +0000

    wrapper script template and manifests

---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+class endpoint_fudge {
+
+  $xinetd_script          = $endpoint_fudge::params::xinetd_script,
+  $xinetd_script_template = $endpoint_fudge::params::xinetd_script_template,
+  $xinetd_user            = $endpoint_fudge::params::xinetd_user,
+  $xinetd_group           = $endpoint_fudge::params::xinetd_group,
+  $xinetd_port            = $endpoint_fudge::params::xinetd_port,
+  $console_api_port       = $endpoint_fudge::params::console_api_port,
+  $console_api_endpoint   = $endpoint_fudge::params::console_api_endpoint,
+
+  file { $xinetd_script:
+    ensure  => file,
+    owner   => $xinetd_user,
+    group   => $xinetd_group,
+    mode    => '700',
+    content => template($xinetd_script_template),
+  }
+
+  xinetd::service { 'endpoint_fudge':
+    port        => $xinetd_port,
+    protocol    => 'tcp',
+    server      => $xinetd_script,
+    user        => $xinetd_user,
+    group       => $xinetd_group,
+    socket_type => 'stream',
+    wait        => 'no',
+    require     => File[ $xinetd_script ],
+  }
+
+}
---
+++ manifests/params.pp
@@ -0,0 +1,9 @@
+class endpoint_fudge::params {
+    $console_api_endpoint   = 'classifier-api/v1/classes'
+    $console_api_port       = '4433'
+	$xinetd_port            = '4555'
+	$xinetd_script          = '/opt/get_classes.sh'
+    $xinetd_script_template = 'endpoint_fudge/api_wrapper.sh.erb'
+    $xinetd_user            = 'root'
+    $xinetd_group           = 'root'
+}
